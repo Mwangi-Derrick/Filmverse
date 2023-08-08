c248e7e821e2a83f4dc9a16f86a8f89c5b8bd305
Mwangi-Derrick <mwangiderrick640@gmail.com>
2023-08-08 18:18:38 +0300

    enabled next js server actions

--- next.config.js
+++ next.config.js
@@ -1,8 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     images: {
-        domains: ['image.tmdb.org'],
-      },
+    domains: ['image.tmdb.org'],
+  },
+  experimental: {
+    serverActions: true,
+  },
 }
 
 module.exports = nextConfig
