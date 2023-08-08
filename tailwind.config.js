fe504509ac233b0f9b147bafe3f62e43e356f59b
derrick <mwangiderrick334@gmail.com>
2023-08-08 18:18:05 +0300

    added custom colors"brown"by extebding the default tailwind color utilities.

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,22 @@ module.exports = {
           'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
       },
       screens: {
-        'sm':'334px',
+        'sm': '334px',
+        '3xl':'2000px',
+      },
+      colors: {
+        brown: {
+          50: '#fdf8f6',
+          100: '#f2e8e5',
+          200: '#eaddd7',
+          300: '#e0cec7',
+          400: '#d2bab0',
+          500: '#bfa094',
+          600: '#a18072',
+          700: '#977669',
+          800: '#846358',
+          900: '#43302b',
+        },
       }
     },
   },
