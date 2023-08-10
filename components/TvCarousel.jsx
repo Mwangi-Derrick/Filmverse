f522974733045813fa0f4c474b8cd2fead098ece
derrick <mwangiderrick334@gmail.com>
2023-08-10 17:27:04 +0300

    added a tv carousel component

--- components/TvCarousel.jsx
+++ components/TvCarousel.jsx
@@ -1,14 +1,15 @@
 "use client"
 import React, { useEffect, useRef, useState } from 'react';
 import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/24/solid';
-import Image from 'next/image';
-import Link from 'next/link';
+import TvShowCard from './TvShowCard';
 
-function MovieCarousel({ data,title }) {
-  const baseImageUrl = 'https://image.tmdb.org/t/p/w500/';
+function TvCarousel({ data,title }) {
   const carouselRef = useRef(null);
   const [scrollButton, setScrollBtn] = useState('none');
-  
+
+  const [TvShows, setTvShows] = useState(data);
+  useEffect(() => { setTvShows(data) }, [data])
+  console.log(TvShows)
   useEffect(() => {
     const handleScrollButtons = () => {
       if (carouselRef.current) {
@@ -53,38 +54,32 @@ function MovieCarousel({ data,title }) {
   return (
     <div className='text-white w-[90%] h-fit mx-auto flex flex-col relative group
     my-5'>
-      <div className='pl-[20px] py-2 h-30px text-2xl font-bold uppercase'>{title}</div>
+    <div className='px-[10px] py-2 lg:h-[60px] flex items-center justify-between
+      '><p className='lg:text-3xl font-medium capitalize'
+        >{title}</p> </div>
       {(scrollButton === 'left' || scrollButton === 'both' ) && (
-        <button onClick={scrollLeft} className='w-12 h-12 bg-white
+        <button onClick={scrollLeft} className='w-[60px] h-[60px] bg-white
           items-center hidden group-hover:flex
          justify-center absolute rounded-[50%] top-[45%] left-[-30px] cursor-pointer'>
-          <ChevronLeftIcon className='w-6 h-6 font-extrabold text-black' />
+          <ChevronLeftIcon className='w-7 h-7 font-extrabold stroke-current stroke-1 text-black' />
         </button>
       )}
       {(scrollButton === 'right' || scrollButton === 'both') && (
-        <button onClick={scrollRight} className='w-12 h-12 bg-white
+        <button onClick={scrollRight} className='w-[60px] h-[60px] bg-white
         absolute  top-[45%] right-[-30px] hidden group-hover:flex
          items-center rounded-[50%] justify-center cursor-pointer'>
-          <ChevronRightIcon className='w-6 h-6 font-extrabold text-black' />
+          <ChevronRightIcon className='w-7 h-7 font-extrabold stroke-current stroke-1 text-black' />
         </button>
       )}
       <div 
         className='flex flex-row w-[100%] h-fit overflow-x-hidden overflow-y-hidden
         px-[5px] items-start justify-start  ' ref={carouselRef}>
-        {data.map((item) => (
-          <Link href={`tv/${item.id}-${item.title}?id=${item.id}`}
-             key={item.id}
-            className='mx-[5px] flex flex-col  items-center justify-start h-fit
-           flex-grow-0 flex-shrink-0 basis-[200px]'> 
-            <Image src={`${baseImageUrl}${item.poster_path}`} className=' h-[250px] 
-            select-none rounded-md
-           ' width={200} height={90} alt={`${item.title}`} />
-             <p className='hover:underline pt-3  '>{item?.title}</p>
-          </Link>
-        ))}
+       {
+  TvShows && TvShows.map((show) => <TvShowCard TvShow={show} />)
+    }
       </div>
     </div>
   );
 }
 
-export default MovieCarousel;
+export default TvCarousel;
