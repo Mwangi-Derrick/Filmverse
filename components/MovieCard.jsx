898b27ad5044a37acac531d10243e5ee7035918d
Mwangi-Derrick <mwangiderrick640@gmail.com>
2023-08-14 17:17:21 +0300

    modified the movie title that is to be used in the url

--- components/MovieCard.jsx
+++ components/MovieCard.jsx
@@ -2,21 +2,23 @@ import React from 'react'
 import Image from 'next/image';
 import Link from 'next/link';
 import placeholder from '../public/palceholder.png';
-
+ 
 function MovieCard({movieId,poster,movieTitle}) {
     const baseImageUrl = 'https://image.tmdb.org/t/p/w500/';
+    const movietitle = `${movieTitle}`
+    const formatedTitlle = movietitle.replace(":", "").replace(/\s+/g, '-')
   return (
-    <Link href={`/movie/${movieId}?id=${movieId}`} as={`/movie/${movieId}-${movieTitle}`}
+    <Link href={`/movie/${movieId}?id=${movieId}`} as={`/movie/${movieId}-${formatedTitlle}`}
             className='mx-[5px] flex flex-col snap-end items-center justify-start h-fit
-           flex-grow-0 flex-shrink-0 lg:basis-[200px] sm:max-lg:basis-[150px]'> 
-            <Image src={`${baseImageUrl}${poster}`} className=' lg:h-[250px] 
+           flex-grow-0 flex-shrink-0 lg:basis-[180px] sm:max-lg:basis-[150px]'> 
+            <Image src={`${baseImageUrl}${poster}`} className='lg:h-[250px] 
             sm:max-lg:h-[180px]
             select-none rounded-md bg-cover
            ' width={200} height={90} alt={`${movieTitle}`}
               placeholder={placeholder}
               blurDataURL={placeholder}
             />
-             <p className='hover:underline pt-3 sm:max-lg:hidden'>{movieTitle}</p>
+      <p className='hover:underline pt-3 sm:max-lg:hidden'>{movieTitle}</p>
           </Link>
   )
 }
