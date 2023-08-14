57adbd08765345f9e25c56155eaead95ac5e6259
derrick <mwangiderrick334@gmail.com>
2023-08-14 17:11:57 +0300

    added a tvshow details component

---
+++ components/TvshowDetails.jsx
@@ -0,0 +1,14 @@
+"use client"
+import React, { useState } from 'react'
+
+function TvshowDetails() {
+   const[details,setDetails]= useState(null)
+  return (
+      <div>
+          TvshowDetails
+          
+    </div>
+  )
+}
+
+export default TvshowDetails
