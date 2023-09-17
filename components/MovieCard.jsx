ec392ee538ccd0fef32e961b2c11b63f3d80e699
derrick <mwangiderrick334@gmail.com>
2023-09-17 17:33:47 +0300

    minor fixes

--- components/MovieCard.jsx
+++ components/MovieCard.jsx
@@ -8,17 +8,19 @@ function MovieCard({movieId,poster,movieTitle}) {
     const movietitle = `${movieTitle}`
     const formatedTitlle = movietitle.replace(":", "").replace(/\s+/g, '-')
   return (
-    <Link href={`/movie/${movieId}?id=${movieId}`} as={`/movie/${movieId}-${formatedTitlle}`}
+    <Link style={{ WebkitTapHighlightColor: "rgba(0,0,0,0)" }}
+      href={`/movie/${movieId}?id=${movieId}`} as={`/movie/${movieId}-${formatedTitlle}`}
             className='mx-[5px] flex flex-col snap-end items-center justify-start h-fit
-           flex-grow-0 flex-shrink-0 lg:basis-[180px] sm:max-lg:basis-[150px]'> 
+           flex-grow-0 flex-shrink-0 lg:basis-[180px] sm:max-lg:basis-[150px] group/movie'> 
             <Image src={`${baseImageUrl}${poster}`} className='lg:h-[250px] 
             sm:max-lg:h-[180px]
             select-none rounded-md bg-cover
            ' width={200} height={90} alt={`${movieTitle}`}
               placeholder={placeholder}
               blurDataURL={placeholder}
-            />
-      <p className='hover:underline pt-3 sm:max-lg:hidden'>{movieTitle}</p>
+      />
+      <div className='w-[180px] h-fit flex justify-center'>
+        <p className='group-hover/movie:underline pt-3 sm:max-lg:hidden truncate'>{movieTitle}</p></div>
           </Link>
   )
 }
