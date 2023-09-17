1f17b1b115c907e9709c3de034f1049ac6872d62
derrick <mwangiderrick334@gmail.com>
2023-09-17 17:30:17 +0300

    👩🏾‍💻some fixes in the rendering logic of the scroll buttons ie if the movie carousel is not scrollable

--- components/MovieCarousel.jsx
+++ components/MovieCarousel.jsx
@@ -50,14 +50,22 @@ function MovieCarousel({ data,title,genreId }) {
       carouselRef.current.scrollLeft -= carouselRef.current.clientWidth / 2;
     }
   };
+  /* function retuens a boolean depending on the scroll width and the client width of the scroll conatainer.
+   This is later used to render the scroll buttons whenever the scroll conatiner
+    is scrollable ie.no hidden overflowing elements*/
+  const movieCarouselScrollable = () => {
+    if (carouselRef.current) {
+      return carouselRef.current.scrollWidth > carouselRef.current.clientWidth
+    }
+  };
   const genreTitle = `${title}`
   let genreHref = genreTitle.replace(/\s+/g,"-")
-  /*an array that contains subtrings of the  title of the carousel, ie we want to check 
+  /*an array that contains subtrings of the  title of the carousel, i.e we want to check 
   if the title starts with the substrings,so as to remove the show more link where appropriate*/
-const possibleStartsWith = ['movies','T']
+const possibleStartsWith = ['movies','topRated','trending','nowPlaying','popular','upcoming']
   const strCondition = possibleStartsWith.some((substring)=>genreHref.startsWith(substring))
   return (
-    <div className='text-white lg:w-[90%] sm:max-lg:w-full h-fit mx-auto flex flex-col relative group
+    <div className='text-white lg:w-[90%] sm:max-lg:w-full h-fit mx-auto flex flex-col 
     my-5'>
       <div className='px-[10px] py-2 lg:h-[60px] flex items-center justify-between
       '><p className='lg:text-3xl font-medium capitalize'
@@ -65,45 +73,43 @@ const possibleStartsWith = ['movies','T']
         {/*conditionally render the show more link based on the starting substring value of 
         the carousel tilte ie.to render the link only for the carousels that contain movie genres */}
         {!strCondition &&(
-          <Link onClick={() => {
-            window.scrollTo({
-              top: 0,
-              behaviour: 'smooth'
-            })
-          }}
+          <Link 
             href={`/movies/${genreHref}-${genreId}/`} className='w-fit h-fit 
-          flex items-center sm:max-lg:text-[11px] font-semibold'>
+          flex items-center sm:max-lg:text-[11px] font-semibold group/genreLink'>
             Show more<i className='flex items-center justify-between w-fit h-fit px-1'>
               <ChevronRightIcon
                 className='lg:h-5 lg:w-5 sm:max-lg:w-3 transition-all duration-900
-                ease-in-out hover:scale-y-[1.1] hover:scale-x-[1.1] hover:translate-x-1
+                ease-in-out group-hover/genreLinkhover:scale-y-[1.1]
+                 group-hover/genreLinkhover:scale-x-[1.1] 
+                 group-hover/genreLink:translate-x-1 group-hover/genreLink:text-red-500
                  sm:max-lg:h-3 stroke-current stroke-1' /></i>
           </Link>)}
       </div>
+<div className='w-full h-fit group relative'>
       {/*conditionally render the scrollbuttons based on the scrollPositon 
       and the number of  items in the scroll container*/}
-      {((scrollButton === 'left' || scrollButton === 'both') && movies.length>6)  && (
+      {((scrollButton === 'left' || scrollButton === 'both') && movieCarouselScrollable()) && (
         <button onClick={scrollLeft} className='w-[60px] h-[60px] bg-white
           items-center sm:hidden lg:group-hover:flex
-         justify-center absolute rounded-[50%] top-[45%] left-[-30px]
+         justify-center absolute rounded-[50%] top-[35%] left-[-30px]
          cursor-pointer '>
           <ChevronLeftIcon className='w-7 h-7 font-extrabold stroke-current stroke-1 text-black' />
         </button>
       )}
-      {((scrollButton === 'right' || scrollButton === 'both') && movies.length>6) && (
+      {((scrollButton === 'right' || scrollButton === 'both') && movieCarouselScrollable() ) && (
         <button onClick={scrollRight} className='w-[60px] h-[60px] bg-white
-        absolute  top-[45%] right-[-30px] sm:hidden lg:group-hover:flex 
+        absolute  top-[35%] right-[-30px] sm:hidden lg:group-hover:flex 
          items-center rounded-[50%] justify-center cursor-pointer '>
           <ChevronRightIcon className='w-7 h-7 font-extrabold stroke-current stroke-1  text-black' />
         </button>
       )}
       <div 
-        className='flex flex-row w-[100%] h-fit lg:overflow-x-hidden overflow-y-hidden
-        px-[5px] items-start justify-start snap-x snap-mandatory ' ref={carouselRef}>
+        className='flex flex-row w-[100%] h-fit lg:overflow-x-hidden overflow-y-hidden 
+        px-[5px] items-start justify-start snap-x snap-mandatory no-scrollbar ' ref={carouselRef}>
         {movies && movies.map((item) => (
           <MovieCard key={item.id} poster={item.poster_path} movieId={item.id} movieTitle={item.title} />
         ))}
-      </div>
+      </div></div>
     </div>
   );
 }
