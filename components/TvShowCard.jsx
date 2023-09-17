c276028ae40d4ffe688897b538b3846f77748502
Mwangi-Derrick <mwangiderrick640@gmail.com>
2023-09-17 17:17:37 +0300

    some minor fixes

--- components/TvShowCard.jsx
+++ components/TvShowCard.jsx
@@ -1,23 +1,32 @@
+"use client"
 import Image from 'next/image'
 import Link from 'next/link';
-import React from 'react'
+import React, { Suspense } from 'react'
 
 const baseImageUrl = 'https://image.tmdb.org/t/p/w500/';
 function TvShowCard({TvShow}) {
   const showTitle = TvShow.name.replace(/\s+/g,"-")
   
   return (
-    <Link href={`tv/${TvShow.id}-${showTitle}`} 
+    <Link style={{ WebkitTapHighlightColor:"rgba(0,0,0,0)"}}
+      href={`/tv/${TvShow.id}-${showTitle}`} 
       className='mx-[5px] flex flex-col snap-end items-center justify-start h-fit
-      flex-grow-0 flex-shrink-0 lg:basis-[180px] sm:max-lg:basis-[150px]'>
+      flex-grow-0 flex-shrink-0 lg:basis-[180px] sm:max-lg:basis-[150px] group/card'>
           <Image width={1900} height={1000}
               src={`${baseImageUrl}${TvShow.poster_path}`} alt={`${TvShow.title}`}
-              layout='responsive' className='lg:h-[250px] 
+        layout='responsive'
+        className='lg:h-[250px] 
             sm:max-lg:h-[180px]
-            select-none rounded-md bg-cover'/>
-          <p className='hover:underline pt-3 sm:max-lg:hidden'>{TvShow.name}</p>  
+            select-none rounded-md bg-cover'
+        placeholder='blur'
+        blurDataURL='./placeholder.png'
+        style={{
+          maxWidth: "100%",
+        height:"auto"}} />
+      <div className='w-[180px] h-fit flex justify-center'>
+        <p className='group-hover/card:underline pt-3 sm:max-lg:hidden truncate'>{TvShow.name}</p></div>
     </Link>
   )
 }
 
-export default TvShowCard
+export default TvShowCard;
