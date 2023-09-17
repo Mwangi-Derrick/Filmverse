5930e6d895b18303a44db882a054d4b14316197d
derrick <mwangiderrick334@gmail.com>
2023-09-17 17:13:27 +0300

    implemented the layout js file in the sign in , forgort password pages/file directories

---
+++ app/forgot-password/layout.js
@@ -0,0 +1,11 @@
+export default async function RootLayout({
+    children,
+  }) {
+    return (
+      <html lang="en">
+        <body className="h-screen bg-zinc-950">
+          {children}
+        </body>
+      </html>
+    )
+  }
