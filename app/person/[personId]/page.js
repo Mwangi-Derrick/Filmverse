d5a771fa76248605c02edc36508bd0c34bd43801
Mwangi-Derrick <mwangiderrick640@gmail.com>
2023-09-28 20:08:33 +0300

    added dynamic metadata

--- app/person/[personId]/page.js
+++ app/person/[personId]/page.js
@@ -1,12 +1,21 @@
 import PersonDetails from '@/components/PersonDetails'
 import React from 'react'
-
+export async function generateMetadata({ params }) {
+  // read route params
+  const person_params = params.personId
+  const subtrings = person_params.split("-")
+  const person_name = subtrings.slice(1,subtrings.length).join("-")
+  return {
+    title: person_name,
+     description:"person-name"
+  }
+}
 export default async function page({ params }) {
     const path = params.personId
     const personId = path.split("-")[0]
     const key = "31893f5365efe0cdf393794446aae7a6"
     const personDetails = 
-    await fetch(`https://api.themoviedb.org/3/person/${personId}?api_key=${key}&append_to_response=combined_credits`)
+    await fetch(`https://api.themoviedb.org/3/person/${personId}?api_key=${key}&append_to_response=combined_credits`,{cache:'no-store'})
   const results = await personDetails.json();
   const { combined_credits, ...others } = results;
   
