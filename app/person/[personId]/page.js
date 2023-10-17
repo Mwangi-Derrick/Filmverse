85222f7e9e2a97562ff0cdf04a6d7eaf01938815
Mwangi-Derrick <mwangiderrick640@gmail.com>
2023-10-17 17:22:08 +0300

    replaced constant local variable data with let to make it global,this enabled access of the actor's biography from the global variable by the generate metadatafunction.

--- app/person/[personId]/page.js
+++ app/person/[personId]/page.js
@@ -1,5 +1,6 @@
 import PersonDetails from '@/components/PersonDetails'
 import React from 'react'
+let results;
 export async function generateMetadata({ params }) {
   // read route params
   const person_params = params.personId
@@ -7,7 +8,7 @@ export async function generateMetadata({ params }) {
   const person_name = subtrings.slice(1,subtrings.length).join("-")
   return {
     title: person_name,
-     description:"person-name"
+     description:`${results?.biography}`
   }
 }
 export default async function page({ params }) {
@@ -16,7 +17,7 @@ export default async function page({ params }) {
     const key = "31893f5365efe0cdf393794446aae7a6"
     const personDetails = 
     await fetch(`https://api.themoviedb.org/3/person/${personId}?api_key=${key}&append_to_response=combined_credits`,{cache:'no-store'})
-  const results = await personDetails.json();
+   results = await personDetails.json();
   const { combined_credits, ...others } = results;
   
   return (
