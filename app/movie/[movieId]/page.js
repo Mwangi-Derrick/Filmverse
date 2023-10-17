5b506e42d92b7b9456f9caa4c4347670a41628d6
Mwangi-Derrick <mwangiderrick640@gmail.com>
2023-10-17 17:20:52 +0300

    replaced constant local variable data with let to make it global,this enabled access of the movie description from the global variable by the generate metadatafunction.

--- app/movie/[movieId]/page.js
+++ app/movie/[movieId]/page.js
@@ -1,12 +1,14 @@
 import MovieDetails from "@/components/MovieDetails";
+let data;
 export async function generateMetadata({ params }) {
   // read route params
   const movie_params = params.movieId
   const subtrings = movie_params.split("-")
-  const movie_title = subtrings.slice(1,subtrings.length).join(" ")
+  const movie_title = subtrings.slice(1, subtrings.length).join(" ")
+  const overview = data?.overview
   return {
     title: movie_title,
-     description:"movie-title"
+     description: `${overview}`
   }
 }
 async function page({ params }) {
@@ -16,7 +18,7 @@ async function page({ params }) {
       const key = '31893f5365efe0cdf393794446aae7a6'
       const results = await
         fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${key}&append_to_response=videos,credits,recommendations`)
-      let data = await results.json();
+       data = await results.json();
       console.log(data)
   return (
     <div>
