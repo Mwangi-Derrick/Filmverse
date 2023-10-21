e9140e0bd6017793b65d5e4e6fa6e26c4ca254ea
Mwangi-Derrick <mwangiderrick640@gmail.com>
2023-10-21 17:01:49 +0300

    removed console.logs

--- app/movie/[movieId]/page.js
+++ app/movie/[movieId]/page.js
@@ -14,12 +14,10 @@ export async function generateMetadata({ params }) {
 async function page({ params }) {
   const idParams = (params.movieId).split("-");
   const movieId = idParams[0];
-  console.log(movieId);
       const key = '31893f5365efe0cdf393794446aae7a6'
       const results = await
         fetch(`https://api.themoviedb.org/3/movie/${movieId}?api_key=${key}&append_to_response=videos,credits,recommendations`)
        data = await results.json();
-      console.log(data)
   return (
     <div>
       <MovieDetails details={data} />
