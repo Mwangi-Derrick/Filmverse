7b55b58dff2f5fbb8ea128f5580605c8a03ede2d
Mwangi-Derrick <mwangiderrick640@gmail.com>
2023-10-30 12:23:25 +0300

    adjested the postionaing,also implemented aspect ratio

--- components/TrailerIframe.jsx
+++ components/TrailerIframe.jsx
@@ -6,7 +6,7 @@ function TrailerIframe({TrailerId,YTtrailer,onClose}) {
         <>{YTtrailer &&(
             <div className="flex flex-col 
             w-fit h-fit bg-black fixed rounded-[15px]
-            md:max-lg:top-[18%] lg:top-[5%] sm:max-md:top-[21%] z-[150]" >
+            top-[5%] z-[150]" >
                 <div className='h-[50px] w-full flex  px-5 items-center justify-between'>
                     <p className='text-xl font-light'>Trailer</p>
                     <i onClick={onClose}
@@ -14,8 +14,8 @@ function TrailerIframe({TrailerId,YTtrailer,onClose}) {
                       lg:hover:bg-stone-800
                       sm:max-lg:bg-stone-800 rounded-[30%]'><XMarkIcon className='h-4 w-4' /></i>
                 </div>
-                <div className='lg:w-[940px] lg:h-[530px] sm:max-md:w-screen sm:max-md:h-[56vw]
-                md:max-lg:w-[640px] md:max-lg:h-[360px]'>
+                <div className='lg:w-[940px] sm:max-md:w-screen 
+                md:max-lg:w-[640px] aspect-video'>
                 <iframe
                     className='w-full h-full'
                     src={`https://www.youtube.com/embed/${TrailerId}`}
