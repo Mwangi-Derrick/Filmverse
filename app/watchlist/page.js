4b16b97b6b092e64700fd636b189e367cf76ea5a
Mwangi-Derrick <mwangiderrick640@gmail.com>
2024-06-07 01:13:22 +0300

    implemented a watchlist page

--- app/watchlist/page.js
+++ app/watchlist/page.js
@@ -1,11 +1,16 @@
 import React from 'react'
-
+export const metadata = {
+   title: "Watchlist",
+   description:" At Fimverse, we understand the importance of movies and TV shows in your life"
+ }
+import Watchlist from '@/components/Watchlist';
 function page() {
-  return (
-      <div>
-          
-    </div>
-  )
-}
-
-export default page
+   return (
+      <div className='flex flex-col items-start justify-start pt-[100px] px-12 gap-2 w-screen min-h-screen'>
+         <Watchlist/>
+      </div>
+     )
+     
+    }
+    
+    export default page
