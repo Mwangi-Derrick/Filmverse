fc406f3b5daa75a6416aa2a1778b58d71289b4b8
Mwangi-Derrick <mwangiderrick640@gmail.com>
2024-06-07 01:15:25 +0300

    wrapped the children with a session provider component

--- app/layout.js
+++ app/layout.js
@@ -3,24 +3,31 @@ import Header from "../components/Header";
 import "./globals.css";
 import { MobileSidebarContextProvider } from "./mobilesidebar_context";
 import MobileSidebar from "@/components/MobileSidebar";
+import { auth } from "@/auth";
 
-export default function RootLayout({ children }) {
+import NextAuthProvider from "@/components/provider/NextAuthProvider";
+
+export default async function RootLayout({ children }) {
+  const session = await auth(); 
+
   return (
     <html lang="en">
       <meta
         name="viewport"
         content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"
-      />
-      <body className="bg-zinc-950 flex flex-col h-full">
-        <MobileSidebarContextProvider>
+      /> <NextAuthProvider session={session}>
+      <body className="bg-zinc-950 flex flex-col h-full">   
+        <MobileSidebarContextProvider> 
           <Header />
           <MobileSidebar />
         </MobileSidebarContextProvider>
         <main className="w-screen h-full min-h-screen">
-          {children}
+            {children}
         </main>
-        <Footer />
-      </body>
+          <Footer />
+         
+        </body>
+        </NextAuthProvider>
     </html>
   );
 }
